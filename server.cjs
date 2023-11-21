6997c5b29afbbd1843c60131737ca8e0cac249a6
SHRAVAN D A <shravan8704@gmail.com>
2023-11-22 01:03:51 +0530

    setup accepted news
    on clicking accept the selected article is send to another endpoint '/api/accept-news'
    the stored article in accepted news can be used to view in aother page or application

--- server.cjs
+++ server.cjs
@@ -4,10 +4,12 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 app.use(cors());
-
+app.use(express.json());
 
 const apiKey = '2c979581b679b06c3cb05f0f114316ce'; 
 const gnewsApiUrl = 'https://gnews.io/api/v4/top-headlines';
+
+let acceptedNews = [];
 
 app.get('/api/news', async (req, res) => {
   try {
@@ -15,7 +17,7 @@ app.get('/api/news', async (req, res) => {
       params: {
         token: apiKey,
         country: 'in',  // Country code for India
-        q: 'Thiruvananthapuram',  // Keyword for Thiruvananthapuram
+        q: 'Kerala',   //Keyword for Thiruvananthapuram
       },
     });
 
@@ -31,6 +33,23 @@ app.get('/api/news', async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
+});
+
+app.post('/api/accept-news', (req, res) => {
+  const acceptedArticle = req.body;
+
+  if (acceptedArticle) {
+    // Handle the accepted article as needed
+    console.log('Accepted News:', acceptedArticle);
+    acceptedNews.push(acceptedArticle);
+    res.status(200).json({ message: 'News accepted successfully' });
+  } else {
+    res.status(400).json({ error: 'Invalid request. No article data received.' });
+  }
+});
+
+app.get('/api/accept-news',(req,res)=>{
+  res.json(acceptedNews);
 });
 
 app.listen(port, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState,useEffect } from 'react'
 
 function App() {
   const[news,setNews] = useState([]);
+  const[loading,setLoading] = useState(true);
+  const[acceptedNews,setAcceptedNews] = useState([])
 
   useEffect(()=>{
     async function fetchNews(){
@@ -12,35 +14,65 @@ function App() {
         const data = await response.json();
         setNews(data);
       } catch(error){
-        console.error(error);
+        console.error("Error Fetching news",error.message);
+      } finally{
+        setLoading(false);
       }
     }
     fetchNews();
   },[]);
 
+  const handleAcceptClick = async (article) =>{
+    try{
+      const response = await fetch('http://localhost:3000/api/accept-news',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify(article),
+      });
+      if (response.ok) {
+        const acceptedArticle = await response.json();
+        console.log('News accepted successfully:', acceptedArticle);
+        // console.log({article})
+      } else {
+        console.error('Error accepting news:', response.statusText);
+      }
+    } catch(error){
+      console.error("Error in accepting news",error.message)
+    }
+
+  };
+
   return (
     <>
       <h1>News:</h1>
-      <table>
-      {news.map(article => (
-        <tr key={article.title}>
-          <td>
-             <img src={article.image} alt="img" width='250px' />
-          </td>
-          <td>
-          {article.title}
-          <p>{article.description}</p>
-          </td>
-        
-        <td>
-          <button>Accept</button>
-        </td>
-        </tr>
-))}
+      {loading ?(
+        <p>Loading. . . </p>
 
-      </table>
+      ):(
+        <table>
+        {news.map(article => (
+          <tr key={article.title}>
+            <td>
+               <img src={article.image} alt="img" width='250px' />
+            </td>
+            <td>
+            {article.title}
+            <p>{article.description}</p>
+            </td>
+          
+          <td>
+            <button onClick={()=> handleAcceptClick(article)} >Accept</button>
+          </td>
+          </tr>
+  ))}
+  
+        </table>
+      )}
+
     </>
-  )
+  );
 }
 
 export default App
