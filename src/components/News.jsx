4f1a828939719911081c524761088265179b07c5
juliaalex811 <jullex81101@gmail.com>
2024-02-14 11:53:12 +0530

    Revert "Merge branch 'main' of https://github.com/FinalProjectCMS/Project_cms"
    This reverts commit 99a16838abb93e66e76d9fb34680fa09dcec41fc, reversing
    changes made to 4d0d880d998f8c9b5b6583f9e9dfbe126698747f.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,77 +1,11 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+
 const News = () => {
-    const[news,setNews] = useState([]);
-  const[loading,setLoading] = useState(true);
-  const[acceptedNews,setAcceptedNews] = useState([])
-
-  useEffect(()=>{
-    async function fetchNews(){
-      try{
-        const response = await fetch('http://localhost:3000/api/news');
-        const data = await response.json();
-        setNews(data);
-      } catch(error){
-        console.error("Error Fetching news",error.message);
-      } finally{
-        setLoading(false);
-      }
-    }
-    fetchNews();
-  },[]);
-
-  const handleAcceptClick = async (article) =>{
-    try{
-      const response = await fetch('http://localhost:3000/api/accept-news',{
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json',
-        },
-        body: JSON.stringify(article),
-      });
-      if (response.ok) {
-        const acceptedArticle = await response.json();
-        console.log('News accepted successfully:', acceptedArticle);
-        // console.log({article})
-      } else {
-        console.error('Error accepting news:', response.statusText);
-      }
-    } catch(error){
-      console.error("Error in accepting news",error.message)
-    }
-
-  };
 
   return (
-    <div>
-        <h1>News:</h1>
-      {loading ?(
-        <p>Loading. . . </p>
-
-      ):(
-        <table>
-        {news.map(article => (
-          <tr key={article.title}>
-            <td>
-               <img src={article.image} alt="img" width='250px' />
-            </td>
-            <td>
-            {article.title}
-            <p>{article.description}</p>
-            <p>Sentiment: {article.sentiment}</p>
-            </td>
-          
-          <td>
-            <button onClick={()=> handleAcceptClick(article)} >Accept</button>
-          </td>
-          </tr>
-  ))}
-  
-        </table>
-        
-      )}
-    </div>
+    
+    <div>News</div>
   )
 }
 
-export default News
+export default News;
